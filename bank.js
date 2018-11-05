877c59379e8daf5a62d63018dc6075bdfeafb409
ericyang97 <ericyang97524@gmail.com>
2018-11-04 22:09:44 -0800

    First three questions finished.

--- bank.js
+++ bank.js
@@ -2,6 +2,10 @@
 var jsonStream = require('duplex-json-stream')
 var net = require('net')
 
+//Intialize an empty transaction log
+var log = [];
+var reducer = function (balance, entry) {return balance + entry.amount}
+
 var server = net.createServer(function(socket) {
   socket = jsonStream(socket)
 
@@ -9,24 +13,37 @@ var server = net.createServer(function(socket) {
     console.log('Bank received:', msg)
     // socket.write can be used to send a reply
     //socket.write('a')
-    switch (command) {
-    case 'balance':
-      console.log()
-      break
 
-    case 'deposit':
-      // ...
-      break
+    switch (msg.cmd) {
+      //Reply with the current balance
+      case 'balance':
+        socket.end({cmd: 'balance', balance: log.reduce(reducer, 0)});
+        break
 
-    default:
-      // Unknown command
-      break
-  }
+      //Push the transaction into log, and reply with the current balance
+      case 'deposit':
+        log.push(msg);
+        socket.end({cmd: 'balance', balance: log.reduce(reducer, 0)});
+        break
 
-    socket.write({cmd: 'balance', balance: 0})
+      case 'withdraw':
+        if(log.reduce(reducer,0) - msg.amount < 0){
+          socket.end({cmd: 'error', balance: 'Not enough amount in the account'})
+        }else {
+          var entry = {cmd: 'withdraw', amount: -msg.amount};
+          log.push(entry);
+          socket.end({cmd: 'balance', balance: log.reduce(reducer, 0)});
+        }
+        break
+
+      //Base case
+      default:
+        socket.end({cmd: 'error', msg: 'Unknown command'})
+        break
+    }
   })
 
 })
 
-console.log("success");
+//console.log("success");
 server.listen(3876)
--- teller.js
+++ teller.js
@@ -4,9 +4,33 @@ var net = require('net')
 
 var client = jsonStream(net.connect(3876))
 
+// Catch the text from command line and discard the first two elements(because first two are execPath and Javascript file path.)
+var argv = process.argv.slice(2)
+
+var command = argv[0]
+
 client.on('data', function (msg) {
-  console.log('Teller received:', msg)
+  console.log('Teller received: ', msg)
 })
 
+switch (command) {
+  case 'deposit':
+    var amount = parseFloat(argv[1])
+    client.end({cmd: 'deposit', amount: amount})
+    break
+
+  case 'balance':
+    client.end({cmd: 'balance'})
+    break
+
+  case 'withdraw':
+    var amount = parseFloat(argv[1])
+    client.end({cmd: 'withdraw', amount: amount})
+    break
+
+  case 'help':
+  default:
+    console.log('node teller.js [CMD]')
+}
 // client.end can be used to send a request and close the socket
-client.end({cmd : 'balance'});
+//client.end({cmd : 'balance'});
